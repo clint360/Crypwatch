fb844a666899461ed902446d0b8d4342f9062525
clint360 <clint360.rebase@gmail.com>
2023-08-22 17:31:33 +0100

    created components

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,61 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from "./Carousel";
+import './Banner.css'
+
+const useStyles = makeStyles((theme) => ({
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className="banner">
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypwatch
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            style={{
+              color: "lightgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
